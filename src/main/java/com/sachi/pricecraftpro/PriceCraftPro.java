52ada105dc10e4edbf15344bbc35a1ae657cf270
NaveenB2004 <naveennbalasooriya2004@gmail.com>
2024-02-09 04:36:55 +0530

    ux

--- src/main/java/com/sachi/pricecraftpro/PriceCraftPro.java
+++ src/main/java/com/sachi/pricecraftpro/PriceCraftPro.java
@@ -3,12 +3,24 @@ package com.sachi.pricecraftpro;
 import com.formdev.flatlaf.FlatDarkLaf;
 import com.sachi.pricecraftpro.helper.DBConnection;
 import com.sachi.pricecraftpro.ui.Home;
+import com.sachi.pricecraftpro.ui.Splash;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class PriceCraftPro {
 
     public static void main(String[] args) {
         FlatDarkLaf.setup();
+        Splash s = new Splash();
+        s.setVisible(true);
         DBConnection.checkDb();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(PriceCraftPro.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+        s.dispose();
         new Home().setVisible(true);
     }
 }
